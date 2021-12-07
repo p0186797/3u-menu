92047f2bd782b92944bc80a09ea59bf3e242593b
p0186797 <76880486+p0186797@users.noreply.github.com>
2021-12-07 13:02:33 -0500

    Create subtract_test.py

---
+++ subtract_test.py
@@ -0,0 +1,7 @@
+import fractions_calculator
+
+a = int(input().strip())
+b = int(input().strip())
+c = int(input().strip())
+d = int(input().strip())
+print(fractions_calculator.subtract(a, b, c, d))
